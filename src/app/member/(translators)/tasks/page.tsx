7ca2838213c6b79dda887c981308ce04b67afad7
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-05-17 16:25:26 +0900

    멤버 수주게시판 반응형 디자인

--- src/app/member/(translators)/tasks/page.tsx
+++ src/app/member/(translators)/tasks/page.tsx
@@ -63,68 +63,47 @@ export default function TasksPage({
 
 function TasksTable({ data }: { data?: any }) {
   return (
-    <section className='py-10 flex flex-col w-full gap-3'>
+    <section className='py-10 flex flex-col w-full gap-3 px-2'>
       <h2 className='text-lg font-semibold pb-8'>번역가 수주 게시판</h2>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 700, width: 150 }} align='center'>
-                번호
-              </TableCell>
-              <TableCell align='center' sx={{ fontWeight: 700 }}>
-                제목
-              </TableCell>
-
-              <TableCell align='center' sx={{ fontWeight: 700, width: 150 }}>
-                링크
-              </TableCell>
-              <TableCell align='center' sx={{ fontWeight: 700, width: 150 }}>
-                언어
-              </TableCell>
-
-              <TableCell align='center' sx={{ fontWeight: 700, width: 230 }}>
-                상태
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data?.tasks?.map((task: TaskType, index: number) => (
-              <TableRow
-                key={`${index}-rows`}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component='th' scope='row' align='center'>
-                  {task.id}
-                </TableCell>
-                <TableCell align='left'>
-                  <Link
-                    className='link link-neutral hover:font-semibold '
-                    href={`/member/tasks/${task.id}`}>
-                    {`${task.title}`}
+      <table>
+        <thead>
+          <tr>
+            <th className='hidden lg:flex'>번호</th>
+            <th>제목</th>
+            <th>링크</th>
+            <th>언어</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.tasks?.map((task: TaskType, index: number) => (
+            <tr key={`${index}-rows`}>
+              <td className='hidden lg:flex'>{task.id}</td>
+              <td>
+                <Link
+                  className='link link-neutral hover:font-semibold '
+                  href={`/member/tasks/${task.id}`}>
+                  {`${task.title}`}
+                </Link>
+              </td>
+              <td>
+                {task.link && (
+                  <Link href={task.link} target='_blank' className='btn btn-sm'>
+                    도서정보
                   </Link>
-                </TableCell>
-                <TableCell align='center'>
-                  {task.link && (
-                    <Link
-                      href={task.link}
-                      target='_blank'
-                      className='btn btn-sm'>
-                      도서정보
-                    </Link>
-                  )}
-                </TableCell>
-                <TableCell align='center'>
-                  <LanguageBadge language={task.language} />
-                </TableCell>
+                )}
+              </td>
+              <td>
+                <LanguageBadge language={task.language} />
+              </td>
 
-                <TableCell align='center'>
-                  <StatusBadge status={task.status} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              <td>
+                <StatusBadge status={task.status} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <TasksPagination count={data?.total_pages ?? 1} />
     </section>
   );
@@ -138,25 +117,25 @@ function StatusBadge({
   switch (status) {
     case 'open':
       return (
-        <div className='border rounded-md font-semibold text-green-700 border-green-700 bg-green-50 flex-1 w-full py-1'>
+        <div className='border rounded-md font-semibold text-green-700 border-green-700 bg-green-50 flex-1 w-full py-1 flex justify-center items-center'>
           모집 중
         </div>
       );
     case 'testing':
       return (
-        <div className='border rounded-md font-semibold text-orange-500 border-orange-500 bg-orange-50 flex-1 w-full py-1'>
+        <div className='border rounded-md font-semibold text-orange-500 border-orange-500 bg-orange-50 flex-1 w-full py-1 flex justify-center items-center'>
           모집 중단 - 샘플심사중
         </div>
       );
     case 'completed':
       return (
-        <div className='border rounded-md font-semibold text-stone-400 border-stone-400 bg-stone-50 flex-1 w-full py-1'>
+        <div className='border rounded-md font-semibold text-stone-400 border-stone-400 bg-stone-50 flex-1 w-full py-1 flex justify-center items-center'>
           마감 - 번역가 선정완료
         </div>
       );
     case 'closed':
       return (
-        <div className='border rounded-md font-semibold text-stone-400 border-stone-400 bg-stone-50 flex-1 w-full py-1'>
+        <div className='border rounded-md font-semibold text-stone-400 border-stone-400 bg-stone-50 flex-1 w-full py-1 flex justify-center items-center'>
           마감 - 작업중단
         </div>
       );
