763ee80320742e8d224b7365c9863aea4e8a17ee
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-05-27 17:31:11 +0900

    introduction/location 반응형

--- src/components/location/DirectionsToOurOffice.tsx
+++ src/components/location/DirectionsToOurOffice.tsx
@@ -7,15 +7,15 @@ export default function DirectionsToOurOffice({}: Props) {
     <Container>
       <section className='flex flex-col'>
         <div className='flex flex-col lg:flex-row gap-4 lg:gap-0'>
-          <div className='flex items-center gap-4 w-[300px]'>
-            <div className='text-white bg-red-800 rounded-full w-10 h-10 flex items-center justify-center text-xl'>
+          <div className='flex items-center gap-2 sm:gap-4 w-[300px]'>
+            <div className='text-white bg-red-800 rounded-full size-5 sm:size-10 flex items-center justify-center text-sm sm:text-xl'>
               6
             </div>
-            <h3 className='text-2xl font-semibold text-slate-700'>
+            <h3 className='text-sm sm:text-2xl font-semibold text-slate-700'>
               상수역 이용
             </h3>
           </div>
-          <ul className='text-slate-500 text-lg'>
+          <ul className='text-slate-500 text-sm sm:text-lg font-thin'>
             <li>
               6호선 상수역 4번 출구로 나오셔서 합정역 방면(출구 방향)으로 4분
               정도 걸어오세요.
@@ -32,20 +32,20 @@ export default function DirectionsToOurOffice({}: Props) {
 
       <section className='flex flex-col'>
         <div className='flex flex-col lg:flex-row gap-4 lg:gap-0'>
-          <div className='flex items-center gap-4 w-[300px] shrink-0 '>
+          <div className='flex items-center gap-2 sm:gap-4 w-[300px] shrink-0 '>
             <div className='flex gap-1 '>
-              <div className='text-white bg-green-600 rounded-full w-10 h-10 flex items-center justify-center text-xl'>
+              <div className='text-white bg-green-800 rounded-full size-5 sm:size-10 flex items-center justify-center text-sm sm:text-xl'>
                 2
               </div>
-              <div className='text-white bg-red-800 rounded-full w-10 h-10 flex items-center justify-center text-xl'>
+              <div className='text-white bg-red-800 rounded-full size-5 sm:size-10 flex items-center justify-center text-sm sm:text-xl'>
                 6
               </div>
             </div>
-            <h3 className='text-2xl font-semibold text-slate-700'>
+            <h3 className='text-sm sm:text-2xl font-semibold text-slate-700'>
               합정역 이용
             </h3>
           </div>
-          <ul className='text-slate-500 text-lg'>
+          <ul className='text-slate-500 text-sm sm:text-lg font-thin'>
             <li>
               6호선 상수역 4번 출구로 나오셔서 합정역 방면(출구 방향)으로 4분
               정도 걸어오세요.
@@ -59,7 +59,7 @@ export default function DirectionsToOurOffice({}: Props) {
         </div>
       </section>
 
-      <ul className='flex flex-col w-full bg-slate-100 px-10 py-8 my-20 text-slate-500'>
+      <ul className='hidden sm:flex flex-col w-full bg-slate-100 px-10 py-8 my-20 text-slate-500'>
         <li>- 입구는 건물의 측면(공인중개사 사무소 옆)에 있습니다.</li>
         <li>- 주차 공간이 부족하오니 되도록 대중교통을 이용해주세요.</li>
       </ul>
--- src/components/location/MapSection.tsx
+++ src/components/location/MapSection.tsx
@@ -7,7 +7,7 @@ type Props = {};
 export default function MapSection({}: Props) {
   return (
     <Container>
-      <div className='border-b flex flex-col'>
+      <div className='flex flex-col'>
         <div className='bg-slate-100 w-full h-[400px]'>
           <Image
             src={img.map}
@@ -16,13 +16,15 @@ export default function MapSection({}: Props) {
             placeholder='blur'
           />
         </div>
-        <div className='w-full flex flex-col lg:flex-row justify-between items-center px-10 my-5 text-slate-700 gap-4'>
-          <p className='text-2xl'>서울시 마포구 어울마당로 26 제일빌딩 5층</p>
-          <ul className='flex divide-x-2'>
-            <li className='px-5'>
+        <div className='w-full flex flex-col lg:flex-row justify-between items-start sm:items-center px-2 sm:px-10 mb-5 py-4 sm:py-10 text-slate-700 gap-4 border border-t-0'>
+          <p className='text-md sm:text-2xl'>
+            서울시 마포구 어울마당로 26 제일빌딩 5층
+          </p>
+          <ul className='flex divide-x-2 font-thin'>
+            <li className='pr-2 sm:px-5 text-sm sm:text-lg'>
               <span className='text-blue-500'>TEL</span> 02-338-2180
             </li>
-            <li className='px-5'>
+            <li className='pl-2 sm:px-5 text-sm sm:text-lg'>
               <span className='text-blue-500'>FAX</span> 02-338-2146
             </li>
           </ul>
